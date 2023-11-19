b205bc32ad81ceca25b115de072bf14963c23316
Ethos <146621450+eth0z@users.noreply.github.com>
2023-11-19 09:16:34 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+/* Making this i realised that i didn't
+have the warning script i made the other day
+because i'm too lazy to go to my onedrive and
+retrieve it so i just made a new one. Feel free to copy it if you want */
+console.log("#     #    #    ######  #     # ### #     #  #####")
+console.log("#  #  #   # #   #     # ##    #  #  ##    # #     #")
+console.log("#  #  #  #   #  #     # # #   #  #  # #   # #      ")
+console.log("#  #  # #     # ######  #  #  #  #  #  #  # #  ####")
+console.log("#  #  # ####### #   #   #   # #  #  #   # # #     #")
+console.log("#  #  # #     # #    #  #    ##  #  #    ## #     #")
+console.log(" ## ##  #     # #     # #     # ### #     #  ##### ")
+console.log("This is a feature intended for developers. If someone has told you to paste text in here, it is likely a scam.")
+console.log("(For what I have no idea)")
